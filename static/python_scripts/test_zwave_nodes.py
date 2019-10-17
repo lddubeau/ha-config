bb35268c7b6175ebb12c199dd6d1545da2d9ffc2
Louis-Dominique Dubeau <ldd@lddubeau.com>
2019-10-17 18:07:03 -0400

    Add logic to manage Z-Wave health.

---
+++ static/python_scripts/test_zwave_nodes.py
@@ -0,0 +1,8 @@
+# The zwave.test_node service does not allow passing multiple node ids. But we
+# want a service that takes multiple node ids. This service bridges the gap, we
+# can pass a single node id or multiple ones as entity_id. The list is then
+# split and passed one by one to zwave.test_node.
+entity_id = data.get('entity_id')
+if entity_id is not None:
+    for entity in entity_id.split(","):
+        hass.services.call('zwave', 'test_node', {'node_id': entity}, False)
